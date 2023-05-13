563a2030394635a0fa5dcb687e9a0c6997399789
Tahlil <bsse0803@iit.du.ac.bd>
2023-05-13 16:18:33 +0600

    Create tronweb obj

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const TronWeb = require('tronweb')
 require('dotenv').config();
 
 
-async function main() {
+async function main(network, amount) {
    try {
-    
+    let url = null;
+    if (network === "shasta") {
+      url = process.env.SHASTA_URL;
+    } else if (network === "nile") {
+      url = process.env.NILE_URL;
+    } else {
+      url = process.env.MAIN_URL;
+    }
+
+    const tronWeb = new TronWeb({
+        fullHost: url
+    });
    } catch (error) {
     console.error(error);
    } 
