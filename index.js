fdfd134b840aa2ba82f73d2c5de1dffceab4bc9e
Tahlil <bsse0803@iit.du.ac.bd>
2023-05-13 18:14:33 +0600

    Add stable coin contract addresses

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const TronWeb = require('tronweb')
 require('dotenv').config();
 
+const contractAddresses = {
+    nile: {
+        USDT: "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj",
+        USDC: "TEMVynQpntMqkPxP6wXTW2K7e4sM3cRmWz"
+    },
+    shasta: {
+        USDT: "TG3XXyExBkPp9nzdajDZsozEu4BkaSJozs",
+        USDC: "TSdZwNqpHofzP6BsBKGQUWdBeJphLmF6id"
+    },
+    main: {
+        USDT: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+        USDC: "TEkxiTehnzSmSe2XqrBj4w32RUN966rdz8"
+    }
+}
 
 async function main(network, recipientAddress, amount) {
    try {
@@ -17,6 +31,8 @@ async function main(network, recipientAddress, amount) {
         fullHost: url,
         privateKey: process.env.PRIVATE_KEY,
     });
+
+    
    } catch (error) {
     console.error(error);
    } 
