dbaed2e66f58ccee77b210c7ce5d85e3c348469c
Tahlil <bsse0803@iit.du.ac.bd>
2023-05-13 19:33:57 +0600

    Send stablecoin

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const TronWeb = require('tronweb')
-require('dotenv').config();
+const TronWeb = require("tronweb");
+require("dotenv").config();
 
 const contractAddresses = {
-    nile: {
-        USDT: "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj",
-        USDC: "TEMVynQpntMqkPxP6wXTW2K7e4sM3cRmWz"
-    },
-    shasta: {
-        USDT: "TG3XXyExBkPp9nzdajDZsozEu4BkaSJozs",
-        USDC: "TSdZwNqpHofzP6BsBKGQUWdBeJphLmF6id"
-    },
-    main: {
-        USDT: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
-        USDC: "TEkxiTehnzSmSe2XqrBj4w32RUN966rdz8"
-    }
-}
+  nile: {
+    USDT: "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj",
+    USDC: "TEMVynQpntMqkPxP6wXTW2K7e4sM3cRmWz",
+  },
+  shasta: {
+    USDT: "TG3XXyExBkPp9nzdajDZsozEu4BkaSJozs",
+    USDC: "TSdZwNqpHofzP6BsBKGQUWdBeJphLmF6id",
+  },
+  main: {
+    USDT: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+    USDC: "TEkxiTehnzSmSe2XqrBj4w32RUN966rdz8",
+  },
+};
 
-async function main(network, recipientAddress, amount) {
-   try {
+async function main(network, stableCoin, toAddress, amount) {
+  try {
+    const fromAddress = process.env.FROM_ADDRESS;
     let url = null;
     if (network === "shasta") {
       url = process.env.SHASTA_URL;
@@ -28,14 +29,43 @@ async function main(network, recipientAddress, amount) {
     }
 
     const tronWeb = new TronWeb({
-        fullHost: url,
-        privateKey: process.env.PRIVATE_KEY,
+      fullHost: url,
+      privateKey: process.env.PRIVATE_KEY,
     });
 
-    
-   } catch (error) {
+    const contractAddress = contractAddresses[network][stableCoin];
+    console.log(contractAddress);
+
+    const options = {
+      feeLimit: 100000000,
+      callValue: 0,
+    };
+    const tx = await tronWeb.transactionBuilder.triggerSmartContract(
+      contractAddress,
+      "transfer(address,uint256)",
+      options,
+      [
+        // {
+        //   type: "address",
+        //   value: fromAddress,
+        // },
+        {
+          type: "address",
+          value: toAddress,
+        },
+        {
+          type: "uint256",
+          value: amount * 1000000,
+        },
+      ],
+      tronWeb.address.toHex(fromAddress)
+    );
+    const signedTx = await tronWeb.trx.sign(tx.transaction);
+    const broadcastTx = await tronWeb.trx.sendRawTransaction(signedTx);
+    console.log(broadcastTx);
+  } catch (error) {
     console.error(error);
-   } 
+  }
 }
 
-main("nile");
+main("nile", "USDT", "TM5eRnvnFwybMfQMdGBFMfNnC2nX7pxA7Y", 11);
